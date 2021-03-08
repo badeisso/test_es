6f962e6647c8cdcc09528321a908a7ca473360b0
Badei Ayasso <bado3ahtc@gmail.com>
2021-03-08 04:58:53 +0200

    Adding code outline and documentation C1M1 Assessment

--- stats.c
+++ stats.c
@@ -10,7 +10,7 @@
  *****************************************************************************/
 /**
  * @file static.c
- * @brief This is first commit
+ * @brief This is second commit
  *
  *
  * @author Badei Ayasso
@@ -18,24 +18,51 @@
  *
  */
 
-
-
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 #include "stats.h"
+#define SIZE 40
 
-/* Size of the Data Set */
-#define SIZE (40)
 
-void main() {
-
-  unsigned char test[SIZE] = { 34, 201, 190, 154,   8, 194,   2,   6,
+void main ()
+{
+     
+     unsigned char test[SIZE] = {34, 201, 190, 154,   8, 194,   2,   6,
                               114, 88,   45,  76, 123,  87,  25,  23,
                               200, 122, 150, 90,   92,  87, 177, 244,
                               201,   6,  12,  60,   8,   2,   5,  67,
                                 7,  87, 250, 230,  99,   3, 100,  90};
-
-  
+    
 
 }
+int find_maximum (unsigned char val[],int length)
+{
+    
+    
+}
+int find_minimum (unsigned char val[],int length)
+{
+    i
+    
+}
+int find_mean(unsigned char val[],int length)
+{
 
-
+}
+int find_median(unsigned char val[],int length)
+{
+    
+}
+void print_statistics(unsigned char val[],int length)
+{
+   
+}
+void print_array(unsigned char val[],int length)
+{
+   
+}
+ int *sort_array(unsigned int val[],int length)
+{
+    
+}
--- stats.h
+++ stats.h
@@ -8,18 +8,116 @@
  * misuse of this material. 
  *
  *****************************************************************************/
- /* @file stats.h 
- * @brief empty header file
- *
- * <Add Extended Description Here>
+/**
+ * @file stats.h 
+ * @brief This file is to be used in the second commit
  *
  * @author Badei Ayasso
  * @date March,4,2021
  *
  */
+
 #ifndef STATS_H_INCLUDED
 #define STATS_H_INCLUDED
 
 
+/**
+ * @brief finds the maximum number of the given array
+ * 
+ * This function takes as an input an unsigned integer array,
+ * and the size of this array.
+ * 
+ * @param unsigned char array,size of the array (40)
+ * @return the maximum integer number in the array
+ */
+ int find_maximum(unsigned char[] ,int);
+
+
+ /**
+ * @brief finds the minimum number of the given array
+ * 
+ * This function takes as an input an unsigned integer array,
+ * and the size of this array.
+ * 
+ * @param unsigned char array,size of the array (40)
+ * @return the minimum integer number in the array
+ */
+ int find_minimum(unsigned char[] ,int);
+
+
+ /**
+ * @brief finds the mean of the given array
+ * 
+ * This function takes as an input an unsigned integer array,
+ * and the size of this array.
+ * adds the elements of array together and divid them
+ * by the number of elements.
+ * 
+ * @param unsigned char array,size of the array (40)
+ * @return a float number of the average value of the array's elements.
+ */
+
+
+ int find_mean(unsigned char[] ,int);
+
+ /**
+ * @brief finds the value of the corresponding index in the given array
+ * 
+ * This function takes as an input an unsigned integer array,
+ * and the size of this array.
+ * if the array has odd number of elements the median is the middle index
+ * if the array has even number of elements the median is the summation of
+ * the two middle indexs divided by 2.
+ * 
+ * @param unsigned char array,size of the array (40)
+ * @return a float value of the midean
+ */
+
+
+ int find_median(unsigned char[] ,int);
+
+ /**
+ * @brief prints the "maximum,minimum,mean,median" values of the given array.
+ * 
+ * This function takes as an input an unsigned integer array,
+ * and the size of this array.
+ * 
+ * 
+ * @param unsigned char array,size of the array (40)
+ * @return no return value
+ */
+
+
+void print_statistics(unsigned char [],int);
+
+/**
+ * @brief prints the elements of unsorted array.
+ * 
+ * This function takes as an input an unsigned integer array,
+ * and the size of this array.
+ * 
+ * 
+ * @param unsigned char array,size of the array (40)
+ * @return no return value
+ */
+
+void print_array(unsigned char [],int);
+
+/**
+ * @brief sorts the elements of an array from the
+ * largest to smallest value
+ * 
+ * This function takes as an input an unsigned integer array,
+ * and the size of this array.
+ * It sorts an array by iterating through its elements,
+ * and swaping the smaller value with the larger one.
+ *
+ * 
+ * @param unsigned integer array,size of the array (40)
+ * @return address of the first element after sorting
+ */
+
+
+ int *sort_array(unsigned int [],int);
 
 #endif // STATS_H_INCLUDED
